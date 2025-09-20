f2a3b70d8cb4ef2693823f92539cecdb3c81911e
arzerra <ianclark1506@gmail.com>
2025-09-21 03:17:14 +0800

    next-app

--- app/users/route.ts
+++ app/users/route.ts
@@ -6,6 +6,17 @@ export const users = [
 export async function GET() {
     return Response.json(users);
 }
-export async function POST() {
-
+export async function POST(request: Request) {
+    const user = await request.json()
+    const newUser = {
+        id: users.length + 1,
+        name: user.name,
+    };
+    users.push(newUser);;
+    return new Response(JSON.stringify(newUser), {
+         headers: {
+            "Conent-Type": "application/json",
+         },
+         status: 201,
+    })
 }
